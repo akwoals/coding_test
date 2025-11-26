db3bd3312423cf06c1b1cfe7060c958f162fa01d
akwoals <107846340+akwoals@users.noreply.github.com>
2025-11-26 16:36:41 +0900

    [level 3] Title: 부서별 평균 연봉 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/284529. 부서별 평균 연봉 조회하기/부서별 평균 연봉 조회하기.sql
@@ -0,0 +1,7 @@
+-- 코드를 작성해주세요
+select hd.dept_id, hd.dept_name_en,round(avg(sal),0) as avg_sal 
+from hr_department as hd
+inner join hr_employees as he on hd.dept_id = he.dept_id
+group by hd.dept_name_en,hd.dept_id
+order by avg_sal desc
+
